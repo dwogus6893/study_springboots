514f6072cb3bb780b573e61c2084eef07a178244
dwogus6893 <dwogus6893@naver.com>
2023-01-05 09:19:01 +0900

    111

---
+++ src/main/java/com/study/yojulab/YojulabApplication.java
@@ -0,0 +1,13 @@
+package com.study.yojulab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YojulabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YojulabApplication.class, args);
+	}
+
+}
