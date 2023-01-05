afa345752632db69c24ef9669c02fe4acca86a5c
dwogus6893 <dwogus6893@naver.com>
2023-01-05 12:35:26 +0900

    222

---
+++ src/main/java/com/study/yojulab/controller/HomeController.java
@@ -0,0 +1,34 @@
+package com.study.yojulab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller //servlets
+public class HomeController {
+    @RequestMapping(value = {"","/","/main"}) //RequestMapping은 function이다.
+    public String main() {
+        int i = 0;
+        return "WEB-INF/views/main.jsp"; 
+    } 
+
+    @RequestMapping(value = "/home") //외부에서 들어오는 url , http://localhost:8080/homejsp
+    public void home() {
+        int i = 0;    
+    } 
+
+    @RequestMapping(value = "/homejsp") //외부에서 들어오는 url , http://localhost:8080/homejsp
+    public String homejsp() {
+        int i = 0;
+        return "WEB-INF/views/home.jsp"; //spring한테 전달하고, view한테 다시 보여줌
+    }
+
+    @RequestMapping(value = "/homehtml") //외부에서 들어오는 url , http://localhost:8080/homejsp
+    public String homehtml() {
+        int i = 0;
+        return "WEB-INF/views/home.html"; //spring한테 전달하고, view한테 다시 보여줌
+    }  
+}
+
