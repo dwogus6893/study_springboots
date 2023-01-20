b0b1725ade8509a49f28878d2f64afc7980c55aa
dwogus6893 <dwogus6893@naver.com>
2023-01-20 10:11:09 +0900

    222

--- src/main/java/com/study/yojulab/controller/CommonCodeOurController.java
+++ src/main/java/com/study/yojulab/controller/CommonCodeOurController.java
@@ -2,18 +2,29 @@ package com.study.yojulab.controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.study.yojulab.service.CommonCodeOurService;
+
+
+
 @Controller
 @RequestMapping(value = "/commonCodeOur")
 
 public class CommonCodeOurController {
+
+    @Autowired
+    CommonCodeOurService commonCodeOurService;
+
     @RequestMapping(value = {"/list","/",""}, method = RequestMethod.GET)
     public ModelAndView list(@RequestParam Map<String, Object> params,ModelAndView modelAndView){
+        Object resultMap = commonCodeOurService.getList(params);
+        modelAndView.addObject(resultMap);
         modelAndView.setViewName("commonCode_our/list");
         return modelAndView;
     }
